5ce2cb3c85273831b7d4b081dd95ef3368b289bd
aasmaaayman <asmaa20171701014@cis.asu.edu.eg>
2021-11-16 11:43:16 +0200

    second commit

--- routes/passportuser.js
+++ routes/passportuser.js
@@ -11,6 +11,6 @@ router.get('/Login', function(req, res, next) {
 });
 // api to check the passport authentication and the passport startegy is jwt session false 3lshan mykonsh leh time b logout b3deh
 router.get('/Payment',passport.authenticate("jwt",{session:false}),(req,res)=>{
-res.send("You have a total of :10000000$");
+res.send("You have a total balance of :10000000$");
 });
 module.exports = router;
